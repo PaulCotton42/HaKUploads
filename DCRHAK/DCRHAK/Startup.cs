7cb36127d84a8a101276371fa0bf2b998b648d3b
PaulCotton42 <54643744+PaulCotton42@users.noreply.github.com>
2019-08-28 13:40:37 -0700

    Add files via upload

---
+++ DCRHAK/DCRHAK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCRHAK.Startup))]
+namespace DCRHAK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
