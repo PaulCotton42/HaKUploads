7d7695520d8241ab30b66d09314d8216f7a36916
PaulCotton42 <54643744+PaulCotton42@users.noreply.github.com>
2019-08-28 13:43:24 -0700

    Add files via upload

---
+++ DCRHAK/DCRHAK/ViewModels/PackageDetailData.cs
@@ -0,0 +1,16 @@
+﻿using DCRHAK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DCRHAK.ViewModels
+{
+    public class PackageDetailData
+    {
+        public IEnumerable<Package> Packages { get; set; }
+        public IEnumerable<Item> Items { get; set; }
+        public IEnumerable<File> Files { get; set; }
+        public IEnumerable<Discussion> Discussions { get; set; }
+    }
+}
